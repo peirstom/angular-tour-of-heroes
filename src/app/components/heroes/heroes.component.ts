487f39b470c5966f590b61f027cfd76b7ef3d60d
Tom Peirs <peirstom@gmail.com>
2021-12-27 01:02:25 +0100

    build angular project

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/interfaces/hero';
+import { HeroService } from 'src/app/services/hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+
+    // the subscribe method waits for the observable to emit the array of heroes, which could happen now or several minutes from now.
+    // the subscribe method passes the emitted array to the callback, which sets the component's heroes property.
+    // This asynchronous approach WILL work when the heroservice requests heroes from the server.
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+}
